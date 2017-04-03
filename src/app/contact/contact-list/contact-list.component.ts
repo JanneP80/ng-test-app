51c7dabd4db4dff3a0337482fb1b632c05850af0
JanneP80 <janne.puolitaival@student.saimia.fi>
2017-04-03 15:04:20 +0300

    update

--- src/app/contact/contact-list/contact-list.component.ts
+++ src/app/contact/contact-list/contact-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Contact} from "../contact";
 
 @Component({
   selector: 'app-contact-list',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactListComponent implements OnInit {
 
-  constructor() { }
+  @Input() contacts: [Contact];
+
+
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
